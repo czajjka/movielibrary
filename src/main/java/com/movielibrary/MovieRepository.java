a8a081260713184c542a09592f63e3d08eff5ea4
Gabi <gabrysia.czajkowska@wp.pl>
2023-12-19 23:42:16 +0100

    Add MovieRepository

---
+++ src/main/java/com/movielibrary/MovieRepository.java
@@ -0,0 +1,22 @@
+package com.movielibrary;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Repository
+public class MovieRepository {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    public List<Movie> getAll(){
+        return jdbcTemplate.query("SELECT id, name, rating FROM movie",
+                BeanPropertyRowMapper.newInstance(Movie.class));
+
+    }
+}
